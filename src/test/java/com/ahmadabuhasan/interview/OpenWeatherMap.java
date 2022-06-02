4c54f188c2f6f3b052f9da880b6cefd65106d0c0
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-02 17:50:06 +0700

    GET API JSON

---
+++ src/test/java/com/ahmadabuhasan/interview/OpenWeatherMap.java
@@ -0,0 +1,34 @@
+package com.ahmadabuhasan.interview;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class OpenWeatherMap {
+
+    @Test
+    public void main() {
+
+        try {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/onecall?&lat=-6.2146&lon=106.8451&exclude=current%2Cminutely%2Chourly%2Calerts&units=metric&appid=73e6d8bf2a4280074fd7f525342d500a");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (conn.getResponseCode() == 200) {
+                Scanner scan = new Scanner(url.openStream());
+                while (scan.hasNext()) {
+                    String temp = scan.nextLine();
+                    //parse json here
+                    System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(new JsonParser().parse(temp)));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
